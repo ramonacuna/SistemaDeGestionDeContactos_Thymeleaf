72449d919540f61fcaffcaaeacc4b568d1c5010f
Ramon Acuña <ramonacuna568@gmail.com>
2026-08-25 08:59:51 -0500

    feat: implementaciond e funcion de editado y eliminacion de contactos existentes en el sistema

--- src/main/java/gm/contactos/controllers/indexController.java
+++ src/main/java/gm/contactos/controllers/indexController.java
@@ -6,9 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,6 +37,28 @@ public class indexController {
     public  String agregar(@ModelAttribute("contactoForm") Contacto contacto) {
         log.info("Agregando contacto: {}", contacto);
         contactoService.save(contacto);
+        return "redirect:/";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String mostrarEditar(@PathVariable("id") Integer id, ModelMap model) {
+        Contacto contacto = contactoService.findById(id);
+        log.info("Contacto recuperado para edición: {}", contacto);
+        model.addAttribute("contactoForm", contacto);
+        return "editar";
+    }
+
+    @PostMapping("/editar/{id}")
+    public String editar(@ModelAttribute("contactoForm") Contacto contacto) {
+        log.info("Editando contacto: {}", contacto);
+        contactoService.save(contacto);
+        return "redirect:/";
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminar(@PathVariable("id") Integer id, ModelMap model) {
+        log.info("Eliminando contacto: {}", contactoService.findById(id));
+        contactoService.deleteById(id);
         return "redirect:/";
     }
 }
